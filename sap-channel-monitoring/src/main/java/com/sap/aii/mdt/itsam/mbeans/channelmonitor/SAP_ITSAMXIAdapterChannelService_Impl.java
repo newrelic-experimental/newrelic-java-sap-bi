244b51decd97c2cda5f76df9b22169a84e0b6dcb
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2024-07-22 09:47:38 -0500

    added channel node monitoring

---
+++ sap-channel-monitoring/src/main/java/com/sap/aii/mdt/itsam/mbeans/channelmonitor/SAP_ITSAMXIAdapterChannelService_Impl.java
@@ -0,0 +1,40 @@
+package com.sap.aii.mdt.itsam.mbeans.channelmonitor;
+
+import com.newrelic.api.agent.Trace;
+import com.newrelic.api.agent.weaver.Weave;
+import com.newrelic.api.agent.weaver.WeaveAllConstructors;
+import com.newrelic.api.agent.weaver.Weaver;
+import com.newrelic.instrumentation.labs.sap.channel.monitoring.ChannelMonitor;
+import com.sap.aii.mdt.itsam.mbeans.channelmonitor.compositedata.SAP_ITSAMXIAdapterChannelList;
+
+@Weave
+public abstract class SAP_ITSAMXIAdapterChannelService_Impl implements SAP_ITSAMXIAdapterChannelService {
+	
+	@WeaveAllConstructors
+	public SAP_ITSAMXIAdapterChannelService_Impl() {
+		if(!ChannelMonitor.initialized) {
+			ChannelMonitor.init();
+		}
+	}
+	
+	
+	@Trace
+	public void StartService() {
+		Weaver.callOriginal();
+	}
+
+	@Trace
+	public void StopService() {
+		Weaver.callOriginal();
+	}
+
+	@Trace
+	public SAP_ITSAMXIAdapterChannelList StartChannels(String[] ChannelIDs, String Locale) {
+		return Weaver.callOriginal();
+	}
+
+	@Trace
+	public SAP_ITSAMXIAdapterChannelList StopChannels(String[] ChannelIDs, String Locale) {
+		return Weaver.callOriginal();
+	}
+}
