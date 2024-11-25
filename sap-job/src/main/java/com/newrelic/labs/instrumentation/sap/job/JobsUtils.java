417d577c3e1ee6b1a21f60b3b826ddd389562fc9
Doug Hilpipre <dhilpipre@users.noreply.github.com>
2024-11-25 14:56:55 -0600

    added module info to spans, fixed DT with QueuedMessages

--- sap-job/src/main/java/com/newrelic/labs/instrumentation/sap/job/JobsUtils.java
+++ sap-job/src/main/java/com/newrelic/labs/instrumentation/sap/job/JobsUtils.java
@@ -1,11 +1,14 @@
 package com.newrelic.labs.instrumentation.sap.job;
 
+import java.util.regex.Pattern;
+
 import com.sap.aii.af.lib.scheduler.Job;
 import com.sap.aii.af.lib.scheduler.Task;
 
 public class JobsUtils {
 	
-
+	private static final Pattern pattern = Pattern.compile("[0-9a-f]+");
+	
 	public static String getJobName(Job job) {
 		String jobName = job.getName();
 		if(jobName == null || jobName.isEmpty()) {
@@ -14,6 +17,13 @@ public class JobsUtils {
 				jobName = task.getClass().getSimpleName();
 			} else {
 				jobName = "UnidentifiedTask";
+			}
+		}
+		int index = jobName.indexOf('_');
+		if(index > -1) {
+			String temp = jobName.substring(index);
+			if(pattern.matcher(temp).matches()) {
+				return jobName.substring(0, index) + "_xxxxx";
 			}
 		}
 		return jobName;
